92b71040d00fc48673e98b674600e768092a2cf3
Aryan7901 <85547188+Aryan7901@users.noreply.github.com>
2022-05-18 18:55:46 +0530

    added title,description

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import { TodoProvider } from "../store/TodoContext";
 import "../styles/globals.css";
-
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <TodoProvider>
+      <Head>
+        <title>Todo List</title>
+        <meta name="description" content="Aryan's Todo List" />
+      </Head>
       <Component {...pageProps} />
     </TodoProvider>
   );
